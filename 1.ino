a356deea035849e97cb594dd2160d4502fdd2e3c
Nanda Vardhan Thupalli <43813351+nandaVT@users.noreply.github.com>
2019-10-18 20:03:11 +0530

    Create 1.ino

---
+++ 1.ino
@@ -0,0 +1,83 @@
+#include <Servo.h>
+
+Servo servo_test;
+
+int pir=A0;
+int trigger=9;
+int echo=10;
+int buzzer=5;
+int angle = 0;
+int anglelimit=15;
+    
+
+void setup() {
+  
+  pinMode(pir,INPUT);
+  pinMode(echo,INPUT);
+  pinMode(trigger,OUTPUT);
+  pinMode(buzzer,OUTPUT);
+  
+  Serial.begin(9600); 
+  servo_test.attach(8);
+
+}
+
+
+  
+void loop() {
+
+
+long duration;
+int distance;
+digitalWrite(trigger, LOW);
+delayMicroseconds(2);
+digitalWrite(trigger, HIGH);
+delayMicroseconds(10);
+digitalWrite(trigger, LOW);
+duration = pulseIn(echo, HIGH);
+distance= duration*0.034/2;
+Serial.print("Distance: ");
+Serial.println(distance);
+
+if(analogRead(pir)<=500){
+  digitalWrite(buzzer,HIGH);
+  delay(100);
+  exit(0);
+}
+else{
+  digitalWrite(buzzer,LOW);
+  delay(100);
+}
+if(angle<180){
+  clockwise();
+}
+else{
+  anticlockwise();
+}
+ 
+
+}
+
+
+  
+
+void anticlockwise(){
+  for(int i = 0; i < anglelimit; i += 1)  
+  {                                  
+    servo_test.write(i);                
+    delay(15);                       
+  } 
+  angle=angle+15;
+  anglelimit=anglelimit+15;
+}
+
+void clockwise(){
+  for(int i = anglelimit; i>=1; i-=5)    
+  {                                
+    servo_test.write(i);              
+    delay(15);                       
+  } 
+  angle=angle-15;
+  anglelimit=anglelimit-15;
+ }
+  
